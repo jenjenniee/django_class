6e53bece15525438ad2f70c9100ed5cdd2feb362
lee jin gyeong <jenjennie707@gmail.com>
2022-04-07 14:56:40 +0900

    4.7

--- blog/models.py
+++ blog/models.py
@@ -5,8 +5,8 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True) #현재시간 자동 저장
-    updated_at = models.DateTimeField(auto_now=True) #다시 저장 할 때마다 그 시간이 저장되게 함
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         # 브라우저 및 어드민에 보이는 형식
